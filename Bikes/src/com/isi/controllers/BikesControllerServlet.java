b98b405d30952d9d6785e96d0e4f09b571b169db
Ximena <ximenacarrilloh@gmail.com>
2022-04-30 14:14:35 -0400

    nuevo proyecto

--- Bikes/src/com/isi/controllers/BikesControllerServlet.java
+++ Bikes/src/com/isi/controllers/BikesControllerServlet.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 import javax.sql.DataSource;
 
